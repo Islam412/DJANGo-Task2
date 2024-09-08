7d00222973ebbc65d9d2d2610581159f74a800f4
Islam412 <islam.124.hamdy@gmail.com>
2024-09-08 20:58:44 +0300

    create student views

--- students/views.py
+++ students/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Student
+from .forms import StudentForm
 
-# Create your views here.
+def student(request):
+    query = request.GET.get('search', '')
+    students = Student.objects.filter(
+        first_name__icontains=query
+    ) | Student.objects.filter(
+        last_name__icontains=query
+    ) | Student.objects.filter(
+        age__icontains=query
+    )
+    context = {
+        'students': students,
+        'query': query
+    }
+    return render(request, 'students/home.html', context)
