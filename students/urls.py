b67bff1bc1ce8e182eca6678d1454231b2f8b409
Islam412 <islam.124.hamdy@gmail.com>
2024-09-08 21:03:45 +0300

    create urls off student vieews

---
+++ students/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+
+app_name = 'students'
+
+urlpatterns = [
+    path('', views.student, name='student'),
+    path('add/', views.add_student, name='add_student'),
+    path('edit/<int:pk>/', views.edit_student, name='edit_student'),
+    path('delete/<int:pk>/', views.delete_student, name='delete_student'),
+]
