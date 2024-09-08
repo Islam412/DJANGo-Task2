6c04983f39769b2a79a10494b8cb4e25832ccd92
Islam412 <islam.124.hamdy@gmail.com>
2024-09-08 20:50:33 +0300

    add RegisterView p1

--- userauths/views.py
+++ userauths/views.py
@@ -1,5 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
-def home(request):
-    return render(request,'userauths/home.html')
+from userauths.models import User
+from userauths.forms import UserRegisterForm
+
+def RegisterView(request):
+    if request.user.is_authenticated:
+        messages.warning(request, f"Hey {request.user.username}, you are already logged in")
+        return redirect('students:student')   
+
+    form = UserRegisterForm(request.POST or None)
+    if form.is_valid():
+        user = form.save()
+        first_name = form.cleaned_data.get('first_name')
+        last_name = form.cleaned_data.get('last_name')
+        email = form.cleaned_data.get('email')
+        password = form.cleaned_data.get('password1')
+
+        user = authenticate(email=email, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, f"Hi {request.user.username}, your account has been created successfully.")
+            return redirect('students:student')
+    
+    context = {'form': form}
+    return render(request, 'userauths/sign-up.html', context)
