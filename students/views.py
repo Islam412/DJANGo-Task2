55475ef2c1ffd563789a2fd572a1f542f10344e9
Islam412 <islam.124.hamdy@gmail.com>
2024-09-08 21:00:25 +0300

    create edit student views

--- students/views.py
+++ students/views.py
@@ -28,3 +28,16 @@ def add_student(request):
         form = StudentForm()
     
     return render(request, 'students/add_student.html', {'form': form})
+
+def edit_student(request, pk):
+    student = get_object_or_404(Student, pk=pk)
+    
+    if request.method == 'POST':
+        form = StudentForm(request.POST, request.FILES, instance=student)
+        if form.is_valid():
+            form.save()
+            return redirect('students:student')
+    else:
+        form = StudentForm(instance=student)
+    
+    return render(request, 'students/edit_student.html', {'form': form})
