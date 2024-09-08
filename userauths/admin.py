1c947ffa2e744ca8ec08ae949d14b8d285d40af8
Islam412 <islam.124.hamdy@gmail.com>
2024-09-08 20:49:01 +0300

    call user at admin

--- userauths/admin.py
+++ userauths/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from userauths.models import User
 
 # Register your models here.
+class UserCustomAdmin(admin.ModelAdmin):
+    list_display = ['username', 'first_name', 'last_name', 'email']
+    
+
+admin.site.register(User, UserCustomAdmin)
